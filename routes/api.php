35b9c3071d9ed37e66d829a54caf5032dd186eea
flipfrog <24383461+flipfrog@users.noreply.github.com>
2024-09-02 18:51:01 +0900

    add categories api endpoint.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HeadlineController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('headlines', HeadlineController::class)->except(['create', 'edit']);
+Route::resource('categories', CategoryController::class)->only(['index']);
